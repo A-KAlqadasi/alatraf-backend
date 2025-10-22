e1dc84846614ff2747bf170eb36062ea207fdded
A-KAlqadsi <abdulkareemalqa@gmail.com>
2025-10-22 22:36:32 +0300

    Completing Common folder part in domain layer

---
+++ src/AlatrafClinic.Domain/Common/Entity.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlatrafClinic.Domain.Common;
+
+public abstract class Entity
+{
+    public int Id { get; }
+
+    private readonly List<DomainEvent> _domainEvents = [];
+
+    [NotMapped]
+    public IReadOnlyCollection<DomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected Entity()
+    { }
+
+    protected Entity(int id)
+    {
+        Id = id;
+    }
+
+    public void AddDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void RemoveDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Remove(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+}
