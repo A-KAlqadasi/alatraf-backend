c78d02ff74722fea9cd703fa8788e4e8121a6ce7
A-KAlqadsi <abdulkareemalqa@gmail.com>
2025-10-28 21:42:56 +0300

    editing on stores in Inventory

---
+++ src/AlatrafClinic.Domain/Inventory/Stores/Store.cs
@@ -0,0 +1,61 @@
+﻿using AlatrafClinic.Domain.Common;
+using AlatrafClinic.Domain.Common.Results;
+using AlatrafClinic.Domain.Inventory.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlatrafClinic.Domain.Inventory.Stores;
+
+public class Store : AuditableEntity<int>
+{
+    public string Name { get; private set; } = string.Empty;
+    private readonly List<StoreItemUnit> _storeItemUnits = new();
+    public IReadOnlyCollection<StoreItemUnit> StoreItemUnits => _storeItemUnits.AsReadOnly();
+
+    private Store() { }
+
+    private Store(string name)
+    {
+        Name = name;
+    }
+
+    public static Result<Store> Create(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            return StoreErrors.NameIsRequired;
+
+        return new Store(name);
+    }
+
+    public Result<Updated> Update(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            return StoreErrors.NameIsRequired;
+
+        Name = name;
+        return Result.Updated;
+    }
+
+    public Result<Updated> AddItemUnit(ItemUnit itemUnit, decimal quantity)
+    {
+        if (itemUnit == null)
+            return StoreErrors.ItemUnitIsRequired;
+
+        var existing = _storeItemUnits.FirstOrDefault(siu => siu.ItemUnitId == itemUnit.Id);
+        if (existing is not null)
+        {
+            return existing.Increase(quantity);
+        }
+
+        var newStoreItem = StoreItemUnit.Create(this, itemUnit, quantity);
+        if (newStoreItem.IsError)
+            return newStoreItem.Errors;
+
+        _storeItemUnits.Add(newStoreItem.Value);
+        return Result.Updated;
+    }
+}
+
---
+++ src/AlatrafClinic.Domain/Inventory/Stores/StoreItemErrors.cs
@@ -0,0 +1,35 @@
+﻿using AlatrafClinic.Domain.Common.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlatrafClinic.Domain.Inventory.Stores;
+
+public static class StoreItemUnitErrors
+{
+    public static readonly Error StoreIsRequired =
+        Error.Validation("StoreItem.StoreIsRequired", "Store reference is required.");
+
+    public static readonly Error ItemIsRequired =
+        Error.Validation("StoreItem.ItemIsRequired", "Item reference is required.");
+
+    public static readonly Error InvalidQuantity =
+        Error.Validation("StoreItem.InvalidQuantity", "Quantity must be zero or greater.");
+
+    public static readonly Error InvalidPrice =
+        Error.Validation("StoreItem.InvalidPrice", "Local price must be greater than or equal to zero.");
+
+    public static readonly Error QuantityNotEnough =
+        Error.Validation("StoreItem.QuantityNotEnough", "Not enough quantity in stock.");
+
+    public static readonly Error AlreadyExists =
+        Error.Conflict("StoreItem.AlreadyExists", "This item already exists in the store inventory.");
+
+    public static readonly Error NotFound =
+        Error.NotFound("StoreItem.NotFound", "The store item could not be found.");
+
+    public static readonly Error CannotDelete =
+        Error.Conflict("StoreItem.CannotDelete", "Store item cannot be deleted due to active references.");
+}
