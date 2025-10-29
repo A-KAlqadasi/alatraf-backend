877a7413f767b0fc2f62839650e70a850f7ff327
WaleedAwn <waleed.awn2023@gmail.com>
2025-10-29 04:22:17 +0300

    Complete adding payment entity and moved it to separate aggregate folder

---
+++ src/AlatrafClinic.Domain/Payments/PaymentType .cs
@@ -0,0 +1,8 @@
+namespace AlatrafClinic.Domain.Patients.Payments.Enums;
+
+public enum PaymentType : byte
+{
+    Therapy = 0, // Payment for therapy cards or sessions
+    Repair = 1,  // Payment for repair cards
+    Sales = 2    // Payment for product sales
+}
