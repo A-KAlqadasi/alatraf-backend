926309403c775a67cdc7864dcb69199dddf9ef99
A-KAlqadsi <abdulkareemalqa@gmail.com>
2025-11-21 20:45:18 +0300

    Adding GetAppointments in application layer

--- src/AlatrafClinic.Application/Common/Interfaces/Repositories/IAppointmentRepository.cs
+++ src/AlatrafClinic.Application/Common/Interfaces/Repositories/IAppointmentRepository.cs
@@ -8,4 +8,5 @@ public interface IAppointmentRepository : IGenericRepository<Appointment, int>
     Task<DateTime> GetLastAppointmentDate(CancellationToken ct = default);
     Task<int> GetAppointmentCountByDate(DateTime date, CancellationToken ct = default);
     Task<int> GetAppointmentCountByDateAndPatientType(DateTime date, PatientType patientType, CancellationToken ct = default);
+    Task<IQueryable<Appointment>> GetAppointmentsQueryAsync(CancellationToken ct = default);
 }
