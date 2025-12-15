40bed043e10f54bd5fb8cca4a46c03476278a3f1
A-KAlqadsi <abdulkareemalqa@gmail.com>
2025-12-16 00:22:48 +0300

    Fix section in medical programs creation

--- src/AlatrafClinic.Application/Features/MedicalPrograms/Commands/CreateMedicalProgram/CreateMedicalProgramCommandHandler.cs
+++ src/AlatrafClinic.Application/Features/MedicalPrograms/Commands/CreateMedicalProgram/CreateMedicalProgramCommandHandler.cs
@@ -28,11 +28,17 @@ public class CreateMedicalProgramCommandHandler : IRequestHandler<CreateMedicalP
     }
     public async Task<Result<MedicalProgramDto>> Handle(CreateMedicalProgramCommand command, CancellationToken ct)
     {
-        var isExists = await _context.MedicalPrograms.Include(m=> m.Section).AnyAsync(mp => mp.Name == command.Name, ct);
+        var isExists = await _context.MedicalPrograms.AnyAsync(mp => mp.Name == command.Name, ct);
         if (isExists)
         {
             _logger.LogWarning("Medical program {name} already exists", command.Name);
             return MedicalProgramErrors.NameAlreadyExists;
+        }
+        var section = await _context.Sections.FirstOrDefaultAsync(s => s.Id == command.SectionId, ct);
+        if (section is null)
+        {
+            _logger.LogWarning("Section with Id {sectionId} not found", command.SectionId);
+            return Error.NotFound("SectionNotFound", $"Section with Id {command.SectionId} not found");
         }
 
         var medicalProgramResult = MedicalProgram.Create(command.Name, command.Description, command.SectionId);
@@ -44,6 +50,7 @@ public class CreateMedicalProgramCommandHandler : IRequestHandler<CreateMedicalP
         }
 
         var medicalProgram = medicalProgramResult.Value;
+        medicalProgram.Section = section;
 
         await _context.MedicalPrograms.AddAsync(medicalProgram, ct);
         await _context.SaveChangesAsync(ct);
