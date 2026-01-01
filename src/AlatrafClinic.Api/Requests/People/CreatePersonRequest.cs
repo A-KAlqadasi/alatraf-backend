52a3bd1d4f1d2a9632fa28b9d0464065ad68751c
A-KAlqadsi <abdulkareemalqa@gmail.com>
2026-01-02 01:37:55 +0300

    Adding Person Get and post action methods

---
+++ src/AlatrafClinic.Api/Requests/People/CreatePersonRequest.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AlatrafClinic.Api.Requests.People;
+
+public class CreatePersonRequest
+{
+    [Required(ErrorMessage = "Fullname is required.")]
+    public string Fullname { get; set; } = default!;
+
+    [Required(ErrorMessage ="Birthdate is required")]
+    public DateOnly Birthdate { get; set; }
+    
+    [Required(ErrorMessage = "Phone number is required")]
+    [RegularExpression(@"^(77|78|73|71|70)\d{7}$", ErrorMessage = "Phone number must start with 77, 78, 73, 71, or 70 and be 9 digits long.")]
+    public string Phone { get; set; } = default!;
+
+    [Required(ErrorMessage = "National number is required")]
+    public string NationalNo {get; set; } = default!;
+    [Required(ErrorMessage = "Address is required")]
+    public string Address { get ;set; } = default!;
+    [Required(ErrorMessage = "Gender is required")]
+    public bool Gender {get; set; }
+}
